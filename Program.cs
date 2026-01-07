aed28e74122af9c93b81729389a8369c96e9c294
Damian Arturo Garces Paez <guners86@hotmail.com>
2026-01-06 19:16:04 -0500

    Add basic Hello World program in C#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
