98b1f3a26a4de37121c8975d4bf80abb9a667c73
Damian Arturo Garces Paez <guners86@hotmail.com>
2026-01-06 19:46:33 -0500

    Simplify Program.cs by removing unnecessary structure

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
-using System;
-
-namespace HolaMundo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("¡Hola Mundo!");
-            Console.ReadLine();
-        }
-    }
-}
+Console.WriteLine("¡Hola Mundo!");
